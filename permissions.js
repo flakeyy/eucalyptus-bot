f7d9e0045cf5a8cd7f3ea30e134a7931950c116c
Flakey <dylanlhanna8@gmail.com>
2025-11-03 21:42:04 -0800

    permissions for editing servers

--- permissions.js
+++ permissions.js
@@ -7,7 +7,13 @@ export const PERMISSIONS = {
     READ_NESTS: 1 << 0, //1
     READ_EGGS: 1 << 1, //2
     CREATE_SERVER: 1 << 2, //4
-    ADMINISTRATOR: 1 << 3, //8
+    SUSPEND_OWN_SERVER: 1 << 3, //8
+    UNSUSPEND_OWN_SERVER: 1 << 4, //16
+    DELETE_OWN_SERVER: 1 << 5, //32
+    LIST_OWN_SERVERS: 1 << 6, //64
+    EDIT_SERVER_SETTINGS: 1 << 7, //128
+
+    ADMINISTRATOR: 1 << 16, //65536
 }
 
 function hasPermission(user, permission) {
