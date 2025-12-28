a01e9c977e3f49c09eb08b5aff8126c972c9ea12
Dylan <113985058+flakeyy@users.noreply.github.com>
2025-12-28 11:40:51 +1100

    fix spacing

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function setPresence() {
 };
 
 dClient.once(Events.ClientReady, readyClient => {
-  console.log(`${readyClient.user.tag} is live | v${require("./package.json").version}/${commitHash} ${useDev ? " | developer mode" : ""}`);
+  console.log(`${readyClient.user.tag} is live | v${require("./package.json").version}/${commitHash}${useDev ? " | developer mode" : ""}`);
   setPresence();
   setInterval(setPresence, 300000);
 });
